8f67c233280d4439262dcb925cf69fea3364a892
on-xx <choi034@mymail.sim.edu.sg>
2022-07-14 18:03:59 +0800

    Project Created

---
+++ src/js/swiper.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper-hero', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    autoplay: {
+        delay: 3000,
+    }
+  });
+
+  var swiper2 = new Swiper(".swiper-promo", {
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 30,
+
+    loop: true,
+  });
+
+
