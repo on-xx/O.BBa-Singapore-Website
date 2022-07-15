8a22c054552fc4d9b93a09517ded79d290ff70f0
on-xx <choi034@mymail.sim.edu.sg>
2022-07-16 00:19:22 +0800

    updated - responsive and franchise left

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -13,28 +13,33 @@ const swiper = new Swiper('.swiper-hero', {
   }
   });
 
-const swiper2 = new Swiper(".swiper-promo", {
+const swiper4 = new Swiper(".swiper-promo", {
   direction: 'horizontal',
   slidesPerView: 4,
   spaceBetween: 30,
   autoHeight: false,
   loop: true,
 
-  breakpoints: {
-    1200: {
-      slidesPerView : 4
-    },
-    750: {
-      slidesPerView : 3
-    },
-    550: {
-      slidesPerView : 1
-    },
+  autoplay: {
+    delay : 1000,
+  },
 
-    300: {
-      slidesPerView : 1
-    }
-  }
+  // breakpoints: {
+  //   1200: {
+  //     slidesPerView : 4
+  //   },
+  //   750: {
+  //     slidesPerView : 1,
+  //     width: 300,
+  //   },
+  //   550: {
+  //     slidesPerView : 1
+  //   },
+
+  //   300: {
+  //     slidesPerView : 1
+  //   },
+  // }
 });
 
 const swiper3 = new Swiper(".swiper-obba-bbq", {
@@ -48,10 +53,37 @@ const swiper3 = new Swiper(".swiper-obba-bbq", {
     el: '.swiper-pagination-bbq',
   },
 
-  
-
   loop: true,
 });
 
+// const breakpoint = window.matchMedia('(max-width: 500px)');
 
+// const breakpointChecker = function() {
+//   if(breakpoint.matches === true){
+//     if(swiper2 !== undefined) {
+//       swiper2.destroy(true, true);
+//     } else {
+//       return;
+//     } 
+//   } else if(breakpoint.matches === false){
+//     return enableSwiper();
+//   }
+// }
+
+// let swiper2;
+
+// const enableSwiper = function() {
+//   swiper2 = new Swiper(".swiper-promo", {
+//     direction: 'horizontal',
+//     slidesPerView: 'auto',
+//     spaceBetween: 30,
+//     autoHeight: false,
+//     loop: true,
+//     // centeredSlides: true,
+
+//   })
+// };
+
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
 
