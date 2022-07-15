44646b10eff8c6dfb88edfc35c512df61c8d3dde
on-xx <choi034@mymail.sim.edu.sg>
2022-07-15 16:23:32 +0800

    menu files reduced

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,24 +1,40 @@
 const swiper = new Swiper('.swiper-hero', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-    autoplay: {
-        delay: 3000,
-    }
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+      delay: 3000,
+  }
   });
 
 const swiper2 = new Swiper(".swiper-promo", {
   direction: 'horizontal',
   slidesPerView: 4,
   spaceBetween: 30,
-
+  autoHeight: false,
   loop: true,
+
+  breakpoints: {
+    1200: {
+      slidesPerView : 4
+    },
+    750: {
+      slidesPerView : 3
+    },
+    550: {
+      slidesPerView : 1
+    },
+
+    300: {
+      slidesPerView : 1
+    }
+  }
 });
 
 const swiper3 = new Swiper(".swiper-obba-bbq", {
